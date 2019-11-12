76f94d908996349c1d95baff2b07db97d7f0e7a0
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-11-12 14:33:31 +0530

    RegularExpression_Lecture01: abc class added to how object is created...

---
+++ RegularExpression/src/javaBySharadSir/abc.java
@@ -0,0 +1,18 @@
+package javaBySharadSir;
+import java.util.*;
+
+class abc{
+
+    int a=2,b=5;
+
+    void func(){
+        if (a>b){
+            System.out.println("a is greater");
+        }
+    }
+
+    public static void main(String[] args) {
+        abc myObj = new abc();
+        myObj.func();
+    }
+}
